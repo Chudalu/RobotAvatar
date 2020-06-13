debd69ddcfe532cda0b6eeded50e4dd20c87ed73
Chudalu <Chudalu@users.noreply.github.com>
2020-06-13 16:09:38 +0100

    Updates

---
+++ precache-manifest.a97877b886fc18641c12676855957424.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce462b2e9c070736fa71a2bdbf93a94",
+    "url": "/robotavatar/index.html"
+  },
+  {
+    "revision": "41c61db65a1fc5c8c570",
+    "url": "/robotavatar/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "937241145d7d088f9299",
+    "url": "/robotavatar/static/css/main.75b16bfc.chunk.css"
+  },
+  {
+    "revision": "41c61db65a1fc5c8c570",
+    "url": "/robotavatar/static/js/2.0aaa0eba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robotavatar/static/js/2.0aaa0eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937241145d7d088f9299",
+    "url": "/robotavatar/static/js/main.fc3638e2.chunk.js"
+  },
+  {
+    "revision": "dd6d80533e19a469518f",
+    "url": "/robotavatar/static/js/runtime-main.db9503f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robot_avatar/precache-manifest.cbc753078b7d2fcf489ba5508e8625f9.js"
+  "/robotavatar/precache-manifest.a97877b886fc18641c12676855957424.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robot_avatar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robotavatar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
